f6e6f2fee68c0d77e05cd0c41eec41773a7f3766
Nikita Dannik <nikitka.dannik@gmail.com>
2020-12-24 20:32:20 +0300

    update

--- script/script.js
+++ script/script.js
@@ -1,26 +1,18 @@
 let content = document.querySelector('.content');
 let profile = content.querySelector('.profile');
 let profileButton = profile.querySelector('.profile__info-btn');
-let popUp = content.querySelector('.popup');
+let popUp = document.querySelector('.popup');
 let popUpCloseButton = popUp.querySelector('.popup__close-button');
 let profileName = profile.querySelector('.profile__name');
 let profileJob = profile.querySelector('.profile__job');
-// if (profileButton == 0) {
-//   popUp.classList.add('popup_opened');
-// }
-
 
 function popUpOpen() {
   popUp.classList.add('popup_opened');
 }
 
-
-profileButton.addEventListener('click', popUpOpen); 
-
 function popUpClose() {
   popUp.classList.remove('popup_opened');
 }
-
 
 let formElement = popUp.querySelector('.popup__form');
 
@@ -34,5 +26,6 @@ function handleFormSubmit (evt) {
     popUpClose();
 }
 
+profileButton.addEventListener('click', popUpOpen);
 popUpCloseButton.addEventListener('click', popUpClose);
 formElement.addEventListener('submit', handleFormSubmit);
