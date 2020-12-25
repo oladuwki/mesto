0bbbd884a0ea44692f3cc845801f2c8f90b98756
Nikita Dannik <nikitka.dannik@gmail.com>
2020-12-25 16:11:53 +0300

    update

--- script/script.js
+++ script/script.js
@@ -5,25 +5,38 @@ let popUp = document.querySelector('.popup');
 let popUpCloseButton = popUp.querySelector('.popup__close-button');
 let profileName = profile.querySelector('.profile__name');
 let profileJob = profile.querySelector('.profile__job');
+let nameInput = document.querySelector('.popup_value_name');
+let jobInput = document.querySelector('.popup_value_job');
+let formElement = popUp.querySelector('.popup__form');
+const user = {
+  name: "Жак-Ив Кусто",
+  job: "Исследователь океана"
+};
+
+setUser(user);
+
 
 function popUpOpen() {
   popUp.classList.add('popup_opened');
+  nameInput.value = user.name;
+  jobInput.value = user.job;
 }
 
 function popUpClose() {
   popUp.classList.remove('popup_opened');
 }
 
-let formElement = popUp.querySelector('.popup__form');
+function setUser(data) {
+  profileName.textContent = data.name;
+  profileJob.textContent = data.job;
+}
 
 function handleFormSubmit (evt) {
   evt.preventDefault();
-
-    let nameInput = popUp.querySelector('.popup__input_name');
-    let jobInput = popUp.querySelector('.popup__input_job');
-    profileName.textContent = nameInput.value;
-    profileJob.textContent = jobInput.value;
-    popUpClose();
+  user.name = nameInput.value;
+  user.job = jobInput.value;
+  setUser(user);
+  popUpClose();
 }
 
 profileButton.addEventListener('click', popUpOpen);
