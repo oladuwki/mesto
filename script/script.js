da08003430ef05b25f3c30736333c3b764da77d6
Nikita Dannik <nikitka.dannik@gmail.com>
2020-12-26 02:27:28 +0300

    update

--- script/script.js
+++ script/script.js
@@ -8,33 +8,21 @@ let profileJob = profile.querySelector('.profile__job');
 let nameInput = document.querySelector('.popup__input_value_name');
 let jobInput = document.querySelector('.popup__input_value_job');
 let formElement = popUp.querySelector('.popup__form');
-const user = {
-  name: "Жак-Ив Кусто",
-  job: "Исследователь океана"
-};
-
-setUser(user);
 
 function popUpOpen() {
   popUp.classList.add('popup_opened');
-  nameInput.value = user.name;
-  jobInput.value = user.job;
+  nameInput.value = profileName.textContent;
+  jobInput.value = profileJob.textContent;
 }
 
 function popUpClose() {
   popUp.classList.remove('popup_opened');
 }
 
-function setUser(data) {
-  profileName.textContent = data.name;
-  profileJob.textContent = data.job;
-}
-
 function handleFormSubmit (evt) {
   evt.preventDefault();
-  user.name = nameInput.value;
-  user.job = jobInput.value;
-  setUser(user);
+  profileName.textContent = nameInput.value;
+  profileJob.textContent = jobInput.value;
   popUpClose();
 }
 
