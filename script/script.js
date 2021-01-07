6ee9dfc937e29af1ab9ff5b9343329750512d3b7
Nikita Dannik <nikitka.dannik@gmail.com>
2021-01-07 15:13:40 +0300

    update

--- script/script.js
+++ script/script.js
@@ -1,22 +1,34 @@
 let content = document.querySelector('.content');
 let profile = content.querySelector('.profile');
 let profileButton = profile.querySelector('.profile__info-btn');
+let profileAddButton = document.querySelector('.profile__add-button');
 let popUp = document.querySelector('.popup');
-let popUpCloseButton = popUp.querySelector('.popup__close-button');
+let popUpCloseButton = document.querySelectorAll('.popup__close-button');
+let popUpSecond = document.querySelector('.popup__second');
 let profileName = profile.querySelector('.profile__name');
 let profileJob = profile.querySelector('.profile__job');
 let nameInput = document.querySelector('.popup__input_value_name');
 let jobInput = document.querySelector('.popup__input_value_job');
-let formElement = popUp.querySelector('.popup__form');
+let [formElement, formElementSecond] = document.querySelectorAll('.popup__form');
+let placeNameInput = document.querySelector('.popup__input_value_place');
+let linkInput = document.querySelector('.popup__input_value_link');
+let likeButtons = document.querySelectorAll('.element__group');
+const elementsContainer = document.querySelector('.elements');
 
+console.log(popUpCloseButton);
 function popUpOpen() {
   popUp.classList.add('popup_opened');
   nameInput.value = profileName.textContent;
   jobInput.value = profileJob.textContent;
 }
 
+function popUpOpenSecond() {
+  popUpSecond.classList.add('popup_opened');
+}
+
 function popUpClose() {
   popUp.classList.remove('popup_opened');
+  popUpSecond.classList.remove('popup_opened');
 }
 
 function handleFormSubmit (evt) {
@@ -26,6 +38,97 @@ function handleFormSubmit (evt) {
   popUpClose();
 }
 
+function togleLike(evt) {
+  evt.target.classList.toggle('element__group_active');
+  console.log(evt);
+};
+
 profileButton.addEventListener('click', popUpOpen);
-popUpCloseButton.addEventListener('click', popUpClose);
+profileAddButton.addEventListener('click', popUpOpenSecond);
+popUpCloseButton[0].addEventListener('click', popUpClose);
+popUpCloseButton[1].addEventListener('click', popUpClose);
 formElement.addEventListener('submit', handleFormSubmit);
+
+
+console.log(likeButtons);
+
+const initialCards = [
+  {
+    name: 'Архыз',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
+  },
+  {
+    name: 'Челябинская область',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
+  },
+  {
+    name: 'Иваново',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
+  },
+  {
+    name: 'Камчатка',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
+  },
+  {
+    name: 'Холмогорский район',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
+  },
+  {
+    name: 'Байкал',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
+  }
+]; 
+
+
+const cardTemplate = document.querySelector('#template').content;
+const elementsList = document.querySelector('.elements');
+
+//создание шаблона карточки, принимая параметры ссылки и имени
+function getCardTemplate(card){
+  const template = cardTemplate.cloneNode(true);
+
+  template.querySelector('.element__image').src = card.link;
+  template.querySelector('.element__name').textContent = card.name;
+  template.querySelector('.element__group').addEventListener('click', togleLike);
+
+  return template;
+};
+
+
+function insertCards(){ //
+  initialCards.forEach((card) => { //получаем данные из массива
+    appendCard(card);
+  });
+};
+
+insertCards();
+
+function handleFormSubmitSecond(evt) {//форма отправки данных из попапа для добавления картинок
+  evt.preventDefault();
+  const dataCard = {
+    name: placeNameInput.value,
+    link: linkInput.value
+  };
+  createCard(dataCard);
+  popUpClose();
+  formElementSecond.reset();
+}
+
+
+function prependCard(card){//отрисовка карточек в начале
+  const cardTemplate = getCardTemplate(card);
+  elementsList.prepend(cardTemplate);
+};
+
+function appendCard(card){//отрисовка карточек в конце
+  const cardTemplate = getCardTemplate(card);
+  elementsList.append(cardTemplate);
+};
+
+function createCard(card) {//добавление информации о карточках в массив и создание карточки
+  initialCards.unshift(card);
+  prependCard(card);
+};
+
+formElementSecond.addEventListener('submit', handleFormSubmitSecond);//событие нажатия кнопки 'сохранить'
+
